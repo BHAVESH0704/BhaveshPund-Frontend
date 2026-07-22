02bb31bd6c73be40774cc8d7ba4a170b5b83d6e7
Bhavesh0704 <pundbhavesh@gmail.com>
2026-07-22 13:19:42 +0530

    Fix JWT interceptor for public APIs

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api/v1",
-
     headers: {
         "Content-Type": "application/json",
     },
@@ -12,7 +11,11 @@ api.interceptors.request.use((config) => {
 
     const token = localStorage.getItem("token");
 
-    if (token) {
+    if (
+        token &&
+        config.url &&
+        config.url.includes("/admin")
+    ) {
         config.headers.Authorization = `Bearer ${token}`;
     }
 
@@ -20,21 +23,16 @@ api.interceptors.request.use((config) => {
 });
 
 api.interceptors.response.use(
-
     (response) => response,
-
     (error) => {
 
         if (error.response?.status === 401) {
-
             localStorage.removeItem("token");
-
             window.location.href = "/admin/login";
         }
 
         return Promise.reject(error);
     }
-
 );
 
 export default api;
